a7494ebb3d182f6aec83b6e8264aaf02cf6b8c9f
Becky Desrosiers <rn7ena@virginia.edu>
2023-10-27 12:27:36 -0400

    Everything that the package needs to work

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name = "HW09Package",
+	version = "0.1",
+	description = "booklover package.",
+	url = "https://github.com/oatmeelsquares/MyFirstPackage",
+	author = "Becky D.",
+	license = "MIT",
+	packages = ["HW09Package"]
+)
